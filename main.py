9ee86ac10c531d3ac609b705014861ec1f65f45e
dankukennedy <dankukennedy@users.noreply.github.com>
2025-12-18 11:31:05 +0000

    all is weel

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, Path, Body
+from fastapi import FastAPI, Path, Body,Query,Request
 from typing import List
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -33,6 +36,13 @@ async def city_data(student:Student,city:City=Body(...)):
 async def index():
     return {"message": "Success! The API is working."}
 
-@app.get("/hello/{name}/{age}")
-async def hello(name:str=Path(..., min_length=3, max_length=10), age:int=Path(...,ge=1, le=100)):
-    return{"name":name ,"age":age}
+#@app.get("/hello/{name}/{age}")
+#async def hello(name:str=Path(..., min_length=3, max_length=10), age:int=Path(...,ge=1, le=100)):
+ #   return{"name":name ,"age":age}
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/about/{name}", response_class=HTMLResponse)
+async def about(request: Request,name:str):
+    return templates.TemplateResponse("about.html", {"request": request, "name":name})
+
